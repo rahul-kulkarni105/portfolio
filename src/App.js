a379918c8368befb21048caa517672d11748ca7c
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2020-02-17 19:01:51 -0600

    Update to manifest.json - removing unused icons - comments clean up

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './App.css';
 import {
   BrowserRouter,
-  // Switch,
-  // Route,
   Link
 } from "react-router-dom";
 import {
@@ -12,21 +10,13 @@ import {
 } from 'grommet';
 import { Router } from './Router/Router';
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
-
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="app-wrapper">
-        <div className="header-wrapper">
-          <Header className="header-component" background="brand" pad="medium">
+    <div className="container-fluid">
+      <div className="app-wrapper row">
+        <nav className="header-wrapper navbar-fixed-top" role="navigation">
+          <Header className="header-component col-sm-4 col-sm-12" background="brand" pad="medium">
             <Link className="nav-link" to="/portfolio">Home</Link>
             <Link className="nav-link" to="/about">About</Link>
             <Link className="nav-link" to="/skills">Skills</Link>
@@ -34,12 +24,13 @@ const App = () => {
             <Link className="nav-link" to="/education">Education</Link>
             <Link className="nav-link" to="/contact">Contact</Link>
           </Header>
-        </div>
+        </nav>
         <hr />
         <Main pad="large">
           <Router />
         </Main>
       </div>
+    </div>
     </BrowserRouter>
   );
 }
