dfd57379e12035f6e3c490ec18b53ff2acd2d7bf
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2020-02-17 18:37:09 -0600

    Homepage description update - WIP notice

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -4,6 +4,7 @@ export const Landing = () => {
   return (
     <div className="p-4">
       <p>Howdy! Welcome to my Portfolio. My name is Rahul Kulkarni and i am a Javascript Engineer</p>
+      <p>This Portfolio is work in progress. :) </p>
     </div>
   );
 };
