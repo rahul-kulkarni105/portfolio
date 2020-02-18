c8c31204a8dc519fe569aebc0fbb6b166dd3ce9b
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2020-02-17 18:35:30 -0600

    Optional route update for homepage

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -16,7 +16,7 @@ export const Router = () => {
     <Switch>
       <Route exact path="/" component={Landing} />
       <Route path="/portfolio" component={Landing} />
-      <Route path="/portfolio/" component={Landing} />
+      <Route exact path="/portfolio/" component={Landing} />
       <Route path="/about" component={About} />
       <Route path="/skills" component={Skills} />
       <Route path="/work" component={Work} />
