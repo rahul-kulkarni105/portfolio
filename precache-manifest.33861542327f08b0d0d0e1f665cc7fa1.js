ea0697b67ed71c14a94020716e7103fa953a82f3
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2020-02-17 18:01:54 -0600

    Updates

---
+++ precache-manifest.33861542327f08b0d0d0e1f665cc7fa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ae4cf64272463f0d094247c48cb658",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "682f2ba3cbea1d67a07b",
+    "url": "/portfolio/static/css/main.094d1699.chunk.css"
+  },
+  {
+    "revision": "621cc2891bc93e14deca",
+    "url": "/portfolio/static/js/2.ea5ada79.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.ea5ada79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "682f2ba3cbea1d67a07b",
+    "url": "/portfolio/static/js/main.16ee3e06.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
