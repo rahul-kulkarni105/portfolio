39cdff3bd051a4771ced902887fbd17f0fd9c056
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2020-02-17 18:03:53 -0600

    Adding homepage for gh-pages deployment - minor css change for landing page

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 export const Landing = () => {
   return (
-    <p>Howdy! Welcome to my Portfolio. My name is Rahul Kulkarni and i am a Javascript Engineer</p>
+    <div className="p-4">
+      <p>Howdy! Welcome to my Portfolio. My name is Rahul Kulkarni and i am a Javascript Engineer</p>
+    </div>
   );
 };
 
