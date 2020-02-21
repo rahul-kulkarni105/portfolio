7b6d5cc0f923b3e3b9ca3352aedefac630620577
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2020-02-21 15:01:32 -0600

    Adding test-watch script

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,10 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
-import Enzyme, {
-  shallow,
-  mount,
-} from 'enzyme';
+import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({
