2cecf0e28a45cca4bb1e81f830265a26622b326b
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2020-02-23 15:55:39 -0600

    Google Analytics clean up

--- src/Utils/GoogleAnalytics.js
+++ src/Utils/GoogleAnalytics.js
@@ -5,62 +5,66 @@ import ReactGA from 'react-ga';
 import { Route } from 'react-router-dom';
 
 class GoogleAnalytics extends Component {
-    componentDidMount () {
-        this.logPageChange(
-            this.props.location.pathname,
-            this.props.location.search
-        );
-    }
+  componentDidMount () {
+    const {
+      location: {
+        pathname,
+        search,
+      },
+    } = this.props;
+    this.logPageChange(pathname, search);
+  }
 
-    componentDidUpdate ({ location: prevLocation }) {
-        const { location: { pathname, search } } = this.props;
-        const isDifferentPathname = pathname !== prevLocation.pathname;
-        const isDifferentSearch = search !== prevLocation.search;
+  componentDidUpdate ({ location: prevLocation }) {
+    const { location: { pathname, search } } = this.props;
+    const isDifferentPathname = pathname !== prevLocation.pathname;
+    const isDifferentSearch = search !== prevLocation.search;
 
-        if (isDifferentPathname || isDifferentSearch) {
-            this.logPageChange(pathname, search);
-        }
+    if (isDifferentPathname || isDifferentSearch) {
+      this.logPageChange(pathname, search);
     }
+  }
 
-    logPageChange (pathname, search = '') {
-        const page = pathname + search;
-        const { location } = window;
-        ReactGA.set({
-            page,
-            location: `${location.origin}${page}`,
-            ...this.props.options
-        });
-        ReactGA.pageview(page);
-    }
+  logPageChange (pathname, search = '') {
+    const page = pathname + search;
+    const { location } = window;
+    ReactGA.set({
+      page,
+      location: `${location.origin}${page}`,
+      ...this.props.options
+    });
+    ReactGA.pageview(page);
+  }
 
-    render () {
-        return null;
-    }
+  render () {
+    return null;
+  }
 }
 
 GoogleAnalytics.propTypes = {
-    location: PropTypes.shape({
-        pathname: PropTypes.string,
-        search: PropTypes.string
-    }).isRequired,
-    options: PropTypes.object
+  location: PropTypes.shape({
+    pathname: PropTypes.string,
+    search: PropTypes.string
+  }).isRequired,
+  options: PropTypes.object
 };
 
 const RouteTracker = () => <Route component={GoogleAnalytics} />;
 
 const init = (options = {}) => {
-    const isGAEnabled = process.env.NODE_ENV === 'production';
+  const isGAEnabled = process.env.NODE_ENV === 'production';
 
-    if (isGAEnabled) {
-        ReactGA.initialize("UA-158787520-1");
-        ReactGA.pageview('/portfolio');
-    }
+  if (isGAEnabled) {
+    ReactGA.initialize("UA-158787520-1");
+    // More urls can be added here if needed
+    ReactGA.pageview('/portfolio');
+  }
 
-    return isGAEnabled;
+  return isGAEnabled;
 };
 
 export default {
-    GoogleAnalytics,
-    RouteTracker,
-    init
+  GoogleAnalytics,
+  RouteTracker,
+  init
 };
