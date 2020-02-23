ccdef37832ca299e713fb7a707a8b89c5f8cd4ac
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2020-02-22 23:56:52 -0600

    Adding google analytics integration

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import {
 import { Router } from './Router/Router';
 import { NavBar } from './Components/NavBar';
 import { LoadingSpinner } from './Components/LoadingSpinner';
+import GA from './Utils/GoogleAnalytics';
 
 const App = () => (
   <BrowserRouter>
+    { GA.init() && <GA.RouteTracker /> }
     <Suspense fallback={()=> <LoadingSpinner />}>
       <div className="container-fluid">
         <div className="portfolio-wrapper">
