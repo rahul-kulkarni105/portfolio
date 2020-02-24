21500e85cb62122513a655b9f3039057a50d9c27
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2020-02-24 17:15:50 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -34,5 +34,6 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+  
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
