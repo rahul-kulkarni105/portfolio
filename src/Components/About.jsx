1c0ddf56a7d9764197fd53919ff652a2998ab0d3
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2020-02-25 22:43:25 -0600

    About me page update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
 export const About = () => (
-  <section className="p-4">
-    <h1 className="pb-5">About me</h1>
-    <p>Javascript developer with recent experience as a front-end lead. I am passionate about writing good code and continue to explore new standards, features, and technology to improve the way I work.</p>
+  <section className="p-4 about row">
+    <section className="about-content col-sm-5">
+      <h1 className="pb-5">About me</h1>
+      <p className>Javascript developer with experience as a front-end lead. I am passionate about writing good code and continue to explore new standards, features, and technology to improve the way I work.</p>
+    </section>
+    <section className="about-cards col-sm-7">
+
+    </section>
   </section>
 );
