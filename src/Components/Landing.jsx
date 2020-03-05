f05a1c65e79aacaa02ee5031d82b8c8d9a868275
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2020-03-05 16:57:37 -0500

    Typo correction

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Landing = () => {
   return (
     <section className="p-4">
-      <p>Howdy! Welcome to my Portfolio. My name is Rahul Kulkarni and i am a Javascript Engineer</p>
+      <p>Howdy! Welcome to my Portfolio. My name is Rahul Kulkarni and I am a Javascript Engineer</p>
       <p>This Portfolio is work in progress. :) </p>
     </section>
   );
