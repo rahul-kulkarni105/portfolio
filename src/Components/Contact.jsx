1fa5e96b9458d5e9b8a8b291064d06dfbdf58a5d
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2020-03-04 23:41:59 -0500

    Removing duplicate Github and linkedin links from contacts

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -7,26 +7,6 @@ export const Contact = () => (
       <li>
         <a href="mailto:rahulkulkarni0510@gmail.com">Contact via Email</a>
       </li>
-      <li>
-        LinkedIn: @rahul-kulkarni105
-        <a
-          href="https://www.linkedin.com/in/rahul-kulkarni105/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn Profile
-        </a>
-      </li>
-      <li>
-        GitHub: rahul-kulkarni105
-        <a
-          href="https://github.com/rahul-kulkarni105"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github Profile
-        </a>
-      </li>
     </ul>
   </section>
 );
