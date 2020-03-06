a004b2a8658d3b996139535070c5a3547ba4bc31
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2020-03-06 00:59:37 -0500

    Updates

---
+++ precache-manifest.4160c2d55bf44cc129d583932e415aa4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a18593612918d0b9fdf45f304b80d15",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "aa4749caad74aeb22893",
+    "url": "/portfolio/static/css/main.e3fcf66b.chunk.css"
+  },
+  {
+    "revision": "170dee4f7e6c5c2fe068",
+    "url": "/portfolio/static/js/2.dd9d2da6.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/portfolio/static/js/2.dd9d2da6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa4749caad74aeb22893",
+    "url": "/portfolio/static/js/main.9ae80166.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "a7218cf423a41cf49c64e72f34aa2e90",
+    "url": "/portfolio/static/media/bg3-unsplash.a7218cf4.jpg"
+  },
+  {
+    "revision": "567748c2bcd211adff2b4b5aab2c2c91",
+    "url": "/portfolio/static/media/umassd1.567748c2.jpeg"
+  },
+  {
+    "revision": "d02f45ecaa071c9e504e65626c2e65aa",
+    "url": "/portfolio/static/media/ytiet1.d02f45ec.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.058aaf9802d78f78f1564847f6c57975.js"
+  "/portfolio/precache-manifest.4160c2d55bf44cc129d583932e415aa4.js"
 );
 
 self.addEventListener('message', (event) => {
