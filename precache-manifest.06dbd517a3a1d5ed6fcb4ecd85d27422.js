639f617a1c3a63c03e1e082b1f5aca545378038e
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2022-04-03 21:29:06 -0400

    Updates

--- precache-manifest.06dbd517a3a1d5ed6fcb4ecd85d27422.js
+++ precache-manifest.06dbd517a3a1d5ed6fcb4ecd85d27422.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c00b1af6fd569931c04b72f4e28a58da",
+    "revision": "37cdd3a4a394f047428470b6faeef7c9",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "9e8349d311ee44530412",
+    "revision": "0b374f323394d0d346e9",
     "url": "/portfolio/static/css/main.be6e9eb5.chunk.css"
   },
   {
-    "revision": "e97138f36224e8a55408",
-    "url": "/portfolio/static/js/2.f790e020.chunk.js"
+    "revision": "c5d2f01d191177d378b4",
+    "url": "/portfolio/static/js/2.ea8685f6.chunk.js"
   },
   {
     "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
-    "url": "/portfolio/static/js/2.f790e020.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.ea8685f6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9e8349d311ee44530412",
-    "url": "/portfolio/static/js/main.65eaf36f.chunk.js"
+    "revision": "0b374f323394d0d346e9",
+    "url": "/portfolio/static/js/main.b09cbd24.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
